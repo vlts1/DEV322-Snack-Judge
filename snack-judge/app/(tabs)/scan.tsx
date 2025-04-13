55791838b330b4b9a4ec1bac62ebcad1e31cea5d
Vladyslav Tsepenok <vladyslavtsepenok@gmail.com>
2025-04-13 16:43:31 -0700

    Init layout

---
+++ snack-judge/app/(tabs)/scan.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Image, Platform, ScrollView } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function TabTwoScreen() {
+  return (
+    <ScrollView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Explore</ThemedText>
+      </ThemedView>
+      <ThemedText>This app includes example code to help you get started.</ThemedText>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
