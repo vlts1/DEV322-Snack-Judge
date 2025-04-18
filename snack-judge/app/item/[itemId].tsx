ce07cb658d2013fa42609bba200479f8a0a5bd51
Ashmeet-Singh5 <122590891+Ashmeet-Singh5@users.noreply.github.com>
2025-04-17 19:46:59 -0700

    Update [itemId].tsx

--- snack-judge/app/item/[itemId].tsx
+++ snack-judge/app/item/[itemId].tsx
@@ -8,39 +8,56 @@ import {
   useColorScheme,
   TouchableOpacity,
   Image,
+  Dimensions,
 } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import ErrorFetching from '@/components/help_components/ErrorFetching';
 import { Colors } from '@/constants/Colors';
+import { useScanHistory } from '@/hooks/useScanHistory';
+import NutritionTable from '@/components/item/NutritionTable';
+import ForgetScan from '@/components/item/ForgetScan';
+import ItemNotFound from '@/components/help_components/ItemNotFound';
 
 export default function ItemScreen() {
   const route = useRoute();
   const { itemId } = route.params as { itemId: string };
+  const { addToHistory } = useScanHistory();
 
   const colorScheme = useColorScheme();
   const textColor = colorScheme === 'dark' ? Colors.dark.text : Colors.light.text;
 
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState<"NONE" | "NETWORK" | "DB">("NONE");
   const [item, setItem] = useState<any>(null);
   const [ingredientsExpanded, setIngredientsExpanded] = useState(false);
 
   const fetchItem = async () => {
     try {
       setLoading(true);
-      setError(false);
+      setError("NONE");
       const res = await fetch(`https://world.openfoodfacts.net/api/v0/product/${itemId}.json`);
       const data = await res.json();
       if (!data || data.status !== 1) throw new Error('Not found');
+
       setItem(data.product);
+
+      const itemToSave = {
+        barcode: data.product.code,
+        name: data.product.product_name || data.product.product_name_fr || data.product.product_name_en,
+        imageUrl: data.product.image_url,
+        make: data.product.brands || data.product.brands_fr || data.product.brands_en,
+      };
+      addToHistory(itemToSave);
+      
     } catch (err: any) {
       console.error(err);
+
       if (err.message === 'Not found') { 
-        console.log("ITEM NOT IN THE DB");
-        // TODO HANDLE ME
-        console.log(err.status);
+        setError("DB");
       }
-      setError(true);
+      else {
+        setError("NETWORK");
+      }
     } finally {
       setLoading(false);
     }
@@ -50,7 +67,8 @@ export default function ItemScreen() {
     fetchItem();
   }, []);
 
-  if (error) return <ErrorFetching onRetry={fetchItem} />;
+  if (error === "NETWORK") return <ErrorFetching onRetry={fetchItem} />;
+  if (error === "DB") return <ItemNotFound/>;
   if (loading || !item) {
     return (
       <View style={styles.centered}>
@@ -61,7 +79,6 @@ export default function ItemScreen() {
 
   const nutriments = item.nutriments || {};
   const additives = item.additives_tags || [];
-  const servings = item.serving_size || 'Unknown';
   const isBadEco = item.ecoscore_grade === 'e';
   const isUltraProcessed = item.nova_group && item.nova_group === 4;
   const name = item.product_name || item.product_name_fr || item.product_name_en || 'Unknown';
@@ -82,7 +99,7 @@ export default function ItemScreen() {
 
   return (
     <ScrollView style={styles.container}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
          <View style={{flexDirection: "column"}}>
           <Text style={[styles.prodBrand, { color: textColor }]}>{make}</Text>
           <Text style={[styles.prodName, { color: textColor }]}>{name}</Text>
@@ -91,35 +108,12 @@ export default function ItemScreen() {
          <Image
            source={{ uri: item.image_url }}
            style={styles.productImage}
-           resizeMode="contain"
          /> 
       </View>
 
+      <View style={[styles.divider, {marginBottom: 40, marginTop: 40}]}/>
 
-      <View style={styles.divider} />
-      <Text style={[styles.sectionTitle, { color: textColor }]}>Nutrition Facts</Text>
-      <Text style={[styles.paragraph, { color: textColor }]}>Serving Size: {servings}</Text>
-      <View style={styles.table}>
-        {[
-          { label: 'Calories', value: nutriments['energy-kcal'], unit: 'kcal' },
-          { label: 'Protein', value: nutriments.proteins, unit: 'g' },
-          { label: 'Fat', value: nutriments.fat, unit: 'g' },
-          { label: 'Carbohydrates', value: nutriments.carbohydrates, unit: 'g' },
-        ].map((row) => (
-          <View key={row.label} style={styles.row}>
-            <Text
-              style={[styles.cellLabel, (row.label === 'Calories' || row.label === 'Protein') && styles.highlight, { color: textColor }]}
-            >
-              {row.label}
-            </Text>
-            <Text
-              style={[styles.cellValue, (row.label === 'Calories' || row.label === 'Protein') && styles.highlight, { color: textColor }]}
-            >
-              {row.value != null ? `${row.value} ${row.unit}` : 'N/A'}
-            </Text>
-          </View>
-        ))}
-      </View>
+      <NutritionTable {...nutriments} />
 
       <Text style={[styles.sectionTitle, { color: textColor }]}>Ingredients</Text>
       <Text style={[styles.paragraph, { color: textColor }]}>{displayedIngredients}</Text>
@@ -137,7 +131,7 @@ export default function ItemScreen() {
         {item.nova_group && (
           <>
             <Text style={[styles.badge, { backgroundColor: isUltraProcessed ? 'orangered' : '#2980b9' }]}>Nova Group: {item.nova_group}</Text>
-            <Text style={[styles.novaExplanation, { color: textColor }]}> {novaExplanations[item.nova_group] || 'Unknown classification'} </Text>
+            <Text style={[styles.novaExplanation, { color: textColor }]}>{novaExplanations[item.nova_group] || 'Unknown classification'} </Text>
           </>
         )}
         <View style={{ height: 15 }}></View>
@@ -154,28 +148,28 @@ export default function ItemScreen() {
         )}
       </View>
       <Text style={[styles.paragraph, { color: textColor }]}>Packaging: {item.packaging || 'N/A'}</Text>
+
+      <View style={[styles.divider, { marginBottom: 30 }]} />
+      <ForgetScan barcode={item.code}/>
+
       <View style={{ height: 80 }} />
     </ScrollView>
   );
 }
 
+const screenWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
-  productImage: { width: "50%", height: 150 },
-  prodName: { fontSize: 24, fontWeight: '700', marginBottom: 8 },
-  prodBrand: { fontSize: 20, fontWeight: '600', marginBottom: 8 },
+  productImage: { height: 130, width: screenWidth * 0.25, borderRadius: 8 },
+  prodName: { fontSize: 24, fontWeight: '700', marginBottom: 8, maxWidth: screenWidth * 0.5, },  
+  prodBrand: { fontSize: 20, fontWeight: '600', marginBottom: 8, maxWidth: screenWidth * 0.5, },
   divider: { height: 1, backgroundColor: '#ccc', marginVertical: 16, marginTop: 30, opacity: 0.6 },
-  container: { flex: 1, padding: 20, paddingTop: 60 },
+  container: { flex: 1, padding: 20, paddingTop: 30 },
   centered: { flex: 1, justifyContent: 'center', alignItems: 'center' },
   sectionTitle: { fontSize: 20, fontWeight: '600', marginTop: 24, marginBottom: 8 },
   subTitle: { fontSize: 17, fontWeight: '600', marginTop: 12 },
   paragraph: { fontSize: 16, marginBottom: 12, lineHeight: 22 },
-  table: { borderRadius: 8, marginBottom: 16, overflow: 'hidden', borderWidth: 1, borderColor: '#ccc' },
-  row: { flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 10, paddingHorizontal: 16, borderBottomWidth: 1, borderBottomColor: '#eee' },
-  cellLabel: { fontSize: 16, fontWeight: '500' },
-  cellValue: { fontSize: 16 },
-  highlight: { fontWeight: '700', color: '#9b59b6' },
   badgeContainer: { flexDirection: 'row', flexWrap: 'wrap', gap: 8, marginBottom: 12, marginTop: 4 },
   badge: { color: 'white', paddingHorizontal: 10, paddingVertical: 4, borderRadius: 12, fontSize: 14, fontWeight: '600' },
-  novaExplanation: { fontSize: 14, marginTop: 6, marginBottom: 10, maxWidth: '100%', lineHeight: 20 },
+  novaExplanation: { fontSize: 16, marginTop: 6, marginBottom: 10, maxWidth: '100%', lineHeight: 20 },
   expandToggle: { fontSize: 14, fontWeight: '500', marginBottom: 12 },
 });
